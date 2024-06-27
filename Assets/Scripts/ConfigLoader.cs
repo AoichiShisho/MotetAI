edfb3562ea49d749bab470b9ade97706a92f8085
urassh <shuto.1011@icloud.com>
2024-06-28 02:27:19 +0900

    delete: debug.log

--- Assets/Scripts/ConfigLoader.cs
+++ Assets/Scripts/ConfigLoader.cs
@@ -13,12 +13,6 @@ class ConfigLoader : MonoBehaviour
         Debug.Log(json != null);
 
         config = JsonUtility.FromJson<ChatGptConfig>(json);
-        
-        Debug.Log(config.API_URL);
-        Debug.Log(config.API_KEY);
-        Debug.Log(config.MODEL);
-        Debug.Log(config.MAX_TOKENS);
-        Debug.Log(config.TEMPERATURE);
     }
 }
 
