c8a4886f265ed041e89abd93b2d775d3215a91aa
urassh <shuto.1011@icloud.com>
2024-06-28 02:24:24 +0900

    rename: configLoader

--- Assets/Scripts/ChatGPTScripts/ChatGPTInteraction.cs
+++ Assets/Scripts/ChatGPTScripts/ChatGPTInteraction.cs
@@ -5,7 +5,7 @@ using CHATGPT.OpenAI;
 using System.Text.RegularExpressions;
 
 public class ChatGPTInteraction : MonoBehaviour {
-    [SerializeField] private ConfigLoader loader;
+    [SerializeField] private ConfigLoader configLoader;
     [TextArea]
     [SerializeField] private string initialSystemMessage = "これから、あるゲームのシナリオとプレイヤーの行動を渡します。これらのシナリオからプレイヤーの行動を考慮して、その後の物語がどのように展開し、プレイヤーが勝利するか敗北するかを決めてください。また、勝利の場合は最後に「モテる！」と記述してください。以下の詳細に従って出力してください。\n\n## ゲーム内容\n- 引き渡したシナリオは、異性の身の安全が危ぶまれる状況です。\n- ゲームの内容は、引き渡したシナリオにおけるプレイヤーの行動で、その後異性と付き合うことができるかを決め、付き合えたら勝利、付き合えなかったら敗北とする、というものです。\n\n## 指示\n- 渡されたシナリオとプレイヤーの行動から、そのシナリオからどのような行動結果になるか考えてください。\n- そのプレイヤーが最終的に好きな人からどのようなリアクションや返答などが得られるかについて考えてください。\n- 考えるシナリオは少し突飛なものであっても構わないものとする。\n- プレイヤーから行動を取得した時、基本的にはフラれること前提で出力してしまって構いません。本当にモテるやつ、もしくはユーモアの塊みたいなケースにのみモテる、という結果の出力になるような確率にしてください。\n\n## 出力フォーマット\n- アドバイスなどはせず、その行動からどのような物語になるかのみ、出力してください。\n- 勝利の場合、文章の最後に「モテる！」と出力してください。";
 
@@ -16,7 +16,7 @@ public class ChatGPTInteraction : MonoBehaviour {
     [SerializeField] private AnswerUIController answerUIController;
 
     void Start() {
-        chatGPTConnection = new ChatGPTConnection(loader.config, initialSystemMessage);
+        chatGPTConnection = new ChatGPTConnection(configLoader.config, initialSystemMessage);
         answerUIController = GetComponent<AnswerUIController>();
     }
 
