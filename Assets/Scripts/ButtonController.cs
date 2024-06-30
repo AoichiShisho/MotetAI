494720e4017a935fec544267bb604f01908c5234
AoichiShisho <sonakamura.aoichi@gmail.com>
2024-07-01 01:11:30 +0900

    [#111] - feature: bgm, sfx added

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
 
-public class ButtonAnimation : EventTrigger
+public class ButtonController : EventTrigger
 {
     public TextMeshProUGUI buttonText;
 
@@ -14,10 +14,19 @@ public class ButtonAnimation : EventTrigger
     public override void OnPointerDown(PointerEventData data)
     {
         buttonText.transform.position += new Vector3(0.5f, -0.5f, 0);
+        PlayButtonSound();
     }
 
     public override void OnPointerUp(PointerEventData data)
     {
         buttonText.transform.position -= new Vector3(0.5f, -0.5f, 0);
+    }
+
+    void PlayButtonSound()
+    {
+        if (SoundManager.Instance != null)
+        {
+            SoundManager.Instance.PlayButtonSound();
+        }
     }
 }
