e0f549a5141d79768b400c5117a7b7c17f6733a2
urassh <shuto.1011@icloud.com>
2024-06-30 18:04:28 +0900

    fix: string

--- Assets/Scripts/MainGameManager.cs
+++ Assets/Scripts/MainGameManager.cs
@@ -160,11 +160,11 @@ public class MainGameManager : MonoBehaviourPunCallbacks
             //FIXME: このresult判定のテキストうまく行ってない。
             if (result.Contains("モテる"))
             {
-                finalResult = $"{playerName}はモテる！";
+                finalResult = $"はモテる！";
             }
             else if (result.Contains("モテない"))
             {
-                finalResult = $"{playerName}はモテない...";
+                finalResult = $"はモテない...";
             }
             else
             {
